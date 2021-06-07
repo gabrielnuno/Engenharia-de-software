9752f9fe0e2d58e948311309c0eb6902535ae800
gabrielnuno <69973827+gabrielnuno@users.noreply.github.com>
2021-06-06 22:12:58 -0300

    Update Multa.java

--- Multa.java
+++ Multa.java
@@ -12,5 +12,52 @@ public class Multa {
 	public Transferencia transferencia;
 	public Date dataVencimento;
 	public ArrayList<Transferencia> transferencias;
+        
+	   public Multa(double valor, Cliente cliente, Transferencia transferencia, Date dataVencimento, ArrayList<Transferencia> transferencias) {
+        this.valor = valor;
+        this.cliente = cliente;
+        this.transferencia = transferencia;
+        this.dataVencimento = dataVencimento;
+        this.transferencias = transferencias;
+    }
 
+    public double getValor() {
+        return valor;
+    }
+
+    public void setValor(double valor) {
+        this.valor = valor;
+    }
+
+    public Cliente getCliente() {
+        return cliente;
+    }
+
+    public void setCliente(Cliente cliente) {
+        this.cliente = cliente;
+    }
+
+    public Transferencia getTransferencia() {
+        return transferencia;
+    }
+
+    public void setTransferencia(Transferencia transferencia) {
+        this.transferencia = transferencia;
+    }
+
+    public Date getDataVencimento() {
+        return dataVencimento;
+    }
+
+    public void setDataVencimento(Date dataVencimento) {
+        this.dataVencimento = dataVencimento;
+    }
+
+    public ArrayList<Transferencia> getTransferencias() {
+        return transferencias;
+    }
+
+    public void setTransferencias(ArrayList<Transferencia> transferencias) {
+        this.transferencias = transferencias;
+    }
 }
