16586739c31037199bf1d14dc7abf7b3905b3f61
gabrielnuno <69973827+gabrielnuno@users.noreply.github.com>
2021-06-06 22:23:37 -0300

    Update operacoesCaixa.java

--- operacoesCaixa.java
+++ operacoesCaixa.java
@@ -13,5 +13,43 @@ public class operacoesCaixa {
 			}
 		}
 	}
+       public int ProcuraC(Cliente c) {
 
+        for (int i = 0; i < c.length(); i++) {
+            if (c.equals(i)) {  
+                return 1;
+            } else {
+                return 0;
+            }
+        }
+        return 0;
+    }
+
+    public double VerMulta(Cliente c) {
+
+        double multa;
+        ProcuraC(Cliente c);
+
+        if (ProcurarC == 1) {
+           multa= Multa.getMulta();
+            if (multa != 0) {
+                return multa;
+            } else {
+                return 0;
+            }
+        }
+         return 0;
+    }
+
+    public void ImprMul(Cliente c) {
+
+        VerMulta(Cliente c);
+        if (VerMulta.getMulta != 0 || VerMulta.getMulta!= null) {
+            System.out.println("A multa do cliente é" + VerMulta.getMulta );
+        } else if (VerMulta == 0) {
+            System.out.println("não há multa");
+        } else {
+            System.out.println("Cliente não encontrado");
+        }
+    }
 }
